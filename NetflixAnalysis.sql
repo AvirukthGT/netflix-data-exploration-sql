b9defd759625f256e0e1f5b4580b78aca9dc4271
Randxm4est <avirukth@gmail.com>
2025-04-28 20:05:36 +1000

    Solved first 10 queries

--- NetflixAnalysis.sql
+++ NetflixAnalysis.sql
@@ -71,3 +71,43 @@ from netflix
 where type='Movie' and duration is not null
 order by 2 desc
 limit 1
+
+
+--Find Content Added in the Last 5 Years
+select *  from netflix where TO_DATE(date_added,'Month DD, YYYY') >= current_date - interval '5 years';
+
+--Find All Movies/TV Shows by Director 'Rajiv Chilaka'
+
+select type,director,title,rating from (
+select *,trim(UNNEST(STRING_TO_ARRAY(director,','))) as new_director  from netflix
+) as t where new_director='Rajiv Chilaka'
+
+--List All TV Shows with More Than 5 Seasons
+
+select * from netflix where type='TV Show'
+and split_part(duration,' ',1)::int>5
+and  duration LIKE '%Season%'
+
+--Count the Number of Content Items in Each Genre
+select trim(unnest(string_to_array(listed_in,','))) as Genre,count(*) from netflix
+group by 1 
+order by 2 desc
+
+--Find each year and the  numbers of content added on netflix in India .
+
+select to_char(TO_DATE(date_added,'Month DD, YYYY'),'YYYY') as year,count(*) as releases from netflix
+where country ilike '%India%'
+group by 1
+order by year
+
+--List All Movies that are Documentaries
+SELECT * 
+FROM netflix
+WHERE listed_in LIKE '%Documentaries';
+
+--Find All Content Without a Director
+SELECT * 
+FROM netflix
+WHERE director IS NULL;
+
+--
