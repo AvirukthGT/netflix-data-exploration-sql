1a69304f23194c6c61776311a36923790b75f24b
Randxm4est <avirukth@gmail.com>
2025-04-28 17:34:05 +1000

    Solved first 5 queries

--- NetflixAnalysis.sql
+++ NetflixAnalysis.sql
@@ -18,3 +18,56 @@ CREATE TABLE netflix
 
 
 select * from netflix
+
+select count(*) from netflix
+
+
+--Distinct content TYPES
+
+select distinct type from netflix;
+
+--Analysis
+
+--Count the Number of Movies vs TV Shows
+select type,count(*) from netflix
+group by 1
+
+--Find the Most Common Rating for Movies and TV Shows,finding frequence of each rating type
+
+select 
+	type,rating
+from(
+select
+	type,
+	rating,
+	count(*),
+	RANK() over(partition by type order by count(*) desc) as ranking
+from netflix
+group by 1,2
+) as t1 where ranking=1;
+
+--List All Movies Released in a Specific Year (e.g., 2020)
+
+SELECT * 
+FROM netflix
+WHERE release_year = 2020 and type='Movie';
+
+-- Find the Top 5 Countries with the Most Content on Netflix
+
+select 
+	trim(UNNEST(STRING_TO_ARRAY(country,','))) as new_country ,
+	count(*)
+from netflix
+group by 1
+order by 2 desc
+limit 5
+
+--Identify the longest movie
+
+select 
+	title,
+	CAST(REPLACE(duration, ' min', '') AS INTEGER) AS duration_minutes
+from netflix
+where type='Movie' and duration is not null
+order by 2 desc
+limit 1
